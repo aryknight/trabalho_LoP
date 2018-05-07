31a5fb1da17cb73373a0979a034f89c7bcccdfef
aryknight <39055775+aryknight@users.noreply.github.com>
2018-05-07 10:12:35 -0300

    Create etapa1.js

---
+++ etapa1.js
@@ -0,0 +1,11 @@
+// Mova seu mouse na janela ao lado
+// para ver o processing em ação
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  rect(40,40,80,80);
+  ellipse(350,80,80,80);
+}
