c2b76c5d2e330a7b3daa5ee5b99eb7c3b0527db1
aryknight <39055775+aryknight@users.noreply.github.com>
2018-06-04 09:46:51 -0300

    Create Etapa5.js

---
+++ Etapa5.js
@@ -0,0 +1,17 @@
+
+var armadura = 3; 
+var pontos = 0;  
+
+function setup() {
+  createCanvas(400,400);
+
+}
+
+function draw() { 
+  background(120);
+  textSize(16);
+  fill(135,206,235);
+  text("Armadura: "+armadura, 5, 370);
+  text("Pontos: "+pontos, 5, 390);;
+  
+}
